725ce2498efea50b5e3fef6e9c63c4ae1984e2c9
Adam Antkowiak <adam.antkowiak93@gmail.com>
2021-06-02 18:55:26 +0200

    Create basic component structure, create apollo client inside App.js

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// components
+import Chat from './components/Chat';
+// apollo
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function App() {
+const endpoint = 'https://chat.thewidlarzgroup.com/api/graphiql';
+
+const client = new ApolloClient({
+  uri: endpoint,
+  cache: new InMemoryCache(),
+});
+
+client
+  .query(
+    {
+      query: gql`
+        query GetRoomsAndUsers {
+          usersRooms {
+            rooms {
+              id
+              name
+              roomPic
+            }
+            user {
+              email
+              firstName
+              id
+              lastName
+              profilePic
+              role
+            }
+          }
+        }
+      `,
+    },
+    {
+      options: {
+        context: {
+          headers: {
+            authorization: `Bearer ${process.env.TOKEN}`,
+          },
+        },
+      },
+    }
+  )
+  .then((result) => console.log(result));
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>CHAT APP!!!! safdg</Text>
+      <Chat />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +66,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Chat = () => {
+  return (
+    <View>
+      <Text>CHAT!!!</Text>
+    </View>
+  );
+};
+
+export default Chat;
