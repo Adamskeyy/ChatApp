369479f0f7e579f0c53fd7e99efdb56709b4887b
Adam Antkowiak <adam.antkowiak93@gmail.com>
2021-06-02 20:21:42 +0200

    Create environment.js file with secret constants and use them in application (add environment.js to gitignore file)

--- App.js
+++ App.js
@@ -4,54 +4,63 @@ import { StyleSheet, Text, View } from 'react-native';
 // components
 import Chat from './components/Chat';
 // apollo
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  HttpLink,
+  ApolloLink,
+} from '@apollo/client';
+// env constants
+import getEnvVars from './environment';
+const { token, endpoint } = getEnvVars();
 
-const endpoint = 'https://chat.thewidlarzgroup.com/api/graphiql';
+const httpLink = new HttpLink({ uri: endpoint });
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  return forward(operation);
+});
 
 const client = new ApolloClient({
   uri: endpoint,
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 client
-  .query(
-    {
-      query: gql`
-        query GetRoomsAndUsers {
-          usersRooms {
-            rooms {
-              id
-              name
-              roomPic
-            }
-            user {
-              email
-              firstName
-              id
-              lastName
-              profilePic
-              role
-            }
+  .query({
+    query: gql`
+      query GetRoomsAndUsers {
+        usersRooms {
+          rooms {
+            id
+            name
+            roomPic
+          }
+          user {
+            email
+            firstName
+            id
+            lastName
+            profilePic
+            role
           }
         }
-      `,
-    },
-    {
-      options: {
-        context: {
-          headers: {
-            authorization: `Bearer ${process.env.TOKEN}`,
-          },
-        },
-      },
-    }
-  )
+      }
+    `,
+  })
   .then((result) => console.log(result));
 
 const App = () => {
+  console.log(token);
   return (
     <View style={styles.container}>
-      <Text>CHAT APP!!!! safdg</Text>
+      <Text>CHAT APP</Text>
       <Chat />
       <StatusBar style="auto" />
     </View>
